063954c6e1526021c8b1b865c82547af8b63ad21
Davis Garrad <Davis.Garrad@gmail.com>
2022-07-25 22:59:24 -0400

    Added link order to README.md

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # BARE2DEngine
 A bare game engine for 2D games in OpenGL, using SDL and CEGUI. That pretty much sums it up. 
+
+# Compiling with BARE2D
+
+To link BARE2D, you'll need a linking order something like the below:
+
+```libboost_thread.a
+libboost_filesystem.a
+libboost_system.a
+lua5.3
+libBARE2DEngine.a
+CEGUIBase-0
+CEGUIOpenGLRenderer-0
+SDL2
+SDL2_mixer
+SDL2_ttf
+GL
+GLU
+GLEW
+pthread```
--- Source/LuaScript.cpp
+++ Source/LuaScript.cpp
@@ -2,50 +2,46 @@
 
 #include "BAREErrors.hpp"
 
+#include <lua5.3/lua.hpp>
+
 namespace BARE2D {
 
-	LuaScript::LuaScript()
-	{
+	LuaScript::LuaScript() {
 	}
 
-	LuaScript::~LuaScript()
-	{
+	LuaScript::~LuaScript() {
 	}
 
-	LuaScriptContextWrapper::LuaScriptContextWrapper()
-	{
+	LuaScriptContextWrapper::LuaScriptContextWrapper() {
 	}
 
-	LuaScriptContextWrapper::~LuaScriptContextWrapper()
-	{
-		
+	LuaScriptContextWrapper::~LuaScriptContextWrapper() {
 	}
 
-	void LuaScriptContextWrapper::init(lua_State* parentState, LuaScript* script)
-	{
+	void LuaScriptContextWrapper::init(lua_State* parentState, LuaScript* script) {
 		// Set the parent state.
 		m_parent = parentState;
-		
+
 		// Create the self's state, which is actually just a subroutine of the main.
 		createThread();
-		
+
 		// Load and compile the script, saving it for later.
 		loadLua(script->m_script);
 	}
-	
+
 	void LuaScriptContextWrapper::createThread() {
 		// Create the thread, as a subroutine of the parent - the master state/thread.
 		m_state = lua_newthread(m_parent);
 		// Push the state to the registry of the parent, and return the index
 		m_threadReference = luaL_ref(m_parent, LUA_REGISTRYINDEX);
-		
+
 		// Make sure that we have no errors.
 		if(m_threadReference == LUA_REFNIL) {
 			m_state = nullptr;
 			throwError(BAREError::LUA_FAILURE, "Failed to create Lua thread");
 		}
 	}
-	
+
 	void LuaScriptContextWrapper::destroy() {
 		m_completed = true;
 		if(m_state) {
@@ -57,31 +53,31 @@ namespace BARE2D {
 		}
 	}
 
-	void LuaScriptContextWrapper::start()
-	{
+	void LuaScriptContextWrapper::start() {
 		// To start a Lua script, we need to call lua_callk to allow yielding.
-		
+
 		// First, to call lua_callk, we need to push the function (which we earlier stored in the registry) to the stack.
 		// Adds registry[m_scriptReference] to the stack.
 		lua_rawgeti(m_state, LUA_REGISTRYINDEX, m_scriptReference);
-		
+
 		// Then push arguments (of which there are none because it's just an entire script)
-	
+
 		// Now actually call the function, starting the script.
 		int nargs = 0;
 		// This actually runs the script as a coroutine, so it can be yielded, etc.
 		int errorCode = lua_resume(m_state, nullptr, nargs);
-		
+
 		// Check if there were any errors
 		if(errorCode != LUA_OK && errorCode != LUA_YIELD) {
 			// There is some error. Throw, and use lua_tostring to get the error string.
-			throwError(BAREError::LUA_FAILURE, "Failed to run Lua script: Lua reports: Error code " + std::to_string(errorCode) + " - \'" + lua_tostring(m_state, -1));
+			throwError(BAREError::LUA_FAILURE,
+					   "Failed to run Lua script: Lua reports: Error code " + std::to_string(errorCode) + " - \'" +
+						   lua_tostring(m_state, -1));
 			return; // we're done here.
 		}
 	}
 
-	void LuaScriptContextWrapper::update()
-	{
+	void LuaScriptContextWrapper::update() {
 		// Check if the function is already being delayed
 		if(m_yielded) {
 			// Check if there's still a delay left or if we should resume.
@@ -99,7 +95,7 @@ namespace BARE2D {
 			// The function is not yielded (!)as far as we know(!)
 			// Check to make sure of this
 			int status = lua_status(m_state);
-			
+
 			// May as well check for errors
 			if(status != LUA_OK && status != LUA_YIELD) {
 				// Error occurred!
@@ -120,28 +116,30 @@ namespace BARE2D {
 			}
 		}
 	}
-	
+
 	void LuaScriptContextWrapper::loadLua(std::string& luaStr) {
 		// Load the string and collect any error code. Note that this loads the script and pushes it to the stack.
 		int errorCode = luaL_loadstring(m_state, (char*)luaStr.c_str());
-		
+
 		// Check the error.
 		if(errorCode != LUA_OK) {
 			// There is some error. Throw, and use lua_tostring to get the error string.
-			throwError(BAREError::LUA_FAILURE, "Failed to compile Lua string: Lua reports: Error code " + std::to_string(errorCode) + " - \'" + lua_tostring(m_state, -1) + "\'\n\n" + luaStr + "\n\n");
+			throwError(BAREError::LUA_FAILURE,
+					   "Failed to compile Lua string: Lua reports: Error code " + std::to_string(errorCode) + " - \'" +
+						   lua_tostring(m_state, -1) + "\'\n\n" + luaStr + "\n\n");
 			// At this point it makes no sense to set the script reference, as the script... doesn't exist.
 			return; // we're done here.
 		}
-		
+
 		// Now that we've loaded and compiled the script, we need to push it to the registry.
 		// This is a short form of popping from the stack, storing it in the registry, and returning the key.
 		int reference = luaL_ref(m_state, LUA_REGISTRYINDEX);
 		// Set the reference to the index in the registry.
 		m_scriptReference = reference;
 	}
-	
+
 	bool LuaScriptContextWrapper::isCompleted() {
 		return m_completed;
 	}
 
-}
+} // namespace BARE2D
